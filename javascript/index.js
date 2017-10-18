326b8640f1c4fb3a4fa32c1dfa13b8c11f8d2c90
ThriteenLi <a1363252276@126.com>
2017-10-18 13:45:12 +0800

    个人网站建设中

--- javascript/index.js
+++ javascript/index.js
@@ -3,11 +3,27 @@ requirejs.config({
         jquery:"jquery-1.11.2"
     }
 });
-require(["jquery","play"],function($,play){
+require(["jquery","play","changeimg"],function($,play,changeimg){
     setTimeout(function(){
         play();
         setInterval(function(){
             play();
         },2800);
 },1400);
+    var oPortfolio=document.getElementById("portfolio");
+    var aLi=oPortfolio.getElementsByTagName("li");
+    console.log(aLi);
+    for(var i=0;i<aLi.length;i++){
+        aLi[i].style.backgroundImage="url(img/"+(i+1)+".jpg)";
+    }
+    var $contact=$('#contact');
+    var $content=$('.content',$contact);
+    var $num=$('.num',$contact);
+    var $nums=$("li",$num);
+    $nums.on("mouseover",function(){
+        changeimg($(this).index());
+    });
+
+
+
 });
